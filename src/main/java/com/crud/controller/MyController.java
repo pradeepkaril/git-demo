e1c89cd9d565ba0c9a83d005d60154b3cc84c7c9
Pradeep <pradeep.karil2012@gmail.com>
2024-01-25 22:35:33 +0530

    my first commit

---
+++ src/main/java/com/crud/controller/MyController.java
@@ -0,0 +1,14 @@
+package com.crud.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MyController {
+	@GetMapping
+	@ResponseBody
+	public String HealthCheck() {
+		return "All are ok";
+	}
+}
